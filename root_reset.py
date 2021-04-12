90aff84e796e2674c1c522b30d5a9010ae4c1e9a
xoix19 <69050547+xoix19@users.noreply.github.com>
2021-04-12 14:39:22 +0700

    Add files via upload

---
+++ root_reset.py
@@ -0,0 +1,13 @@
+import os,sys,time
+import socket
+
+print ("simple root reset password")
+print ("github:wearexoix19")
+print ("author:xoix19")
+print ("")
+time.sleep(2.0)
+os.system("sudo su")
+os.system("passwd")
+time.sleep(1.0)
+os.system("clear")
+print ("PLEASE RESTART YOUR DEVICE")
